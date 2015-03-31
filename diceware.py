ba4e75ceda97c5d2856992ccf046544fa5d75e35
Kat Matfield <katmatfield@gmail.com>
2015-03-31 21:36:10 +0100

    Module to generate passphrases

---
+++ diceware.py
@@ -0,0 +1,19 @@
+import os
+import string
+
+from diceware_wordlist import wordlist as word_dict
+
+def randomDiceRoll():
+	random_generator = random.SystemRandom()
+	random_list = [str(random_generator.randint(1,6)) for i in range(0,5)]
+	return string.join(random_list,"")
+
+def makePassphrase():
+	return [
+		word_dict[randomDiceRoll()] + " " +
+		word_dict[randomDiceRoll()] + " " +
+		word_dict[randomDiceRoll()] + " " +
+		word_dict[randomDiceRoll()] + " " +
+		word_dict[randomDiceRoll()] + " " +
+		word_dict[randomDiceRoll()]
+		]
